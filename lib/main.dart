72335a486ff9d54751727cf79feaa629a07f2832
Marwanhoo <muh.marwanhoo@gmail.com>
2024-06-29 11:08:11 +0300

    add multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_notes_app_hive/constants/constants.dart';
+import 'package:flutter_notes_app_hive/cubits/add_note/add_note_cubit.dart';
 import 'package:flutter_notes_app_hive/models/note_model.dart';
 import 'package:flutter_notes_app_hive/views/notes_view.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -17,19 +19,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'notes',
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=> AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        title: 'notes',
+        theme: ThemeData(
+          useMaterial3: true,
+          brightness: Brightness.light,
+        ),
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          brightness: Brightness.dark,
+          fontFamily: "Poppins",
+        ),
+        themeMode: ThemeMode.dark,
+        home: const NotesView(),
       ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
-        fontFamily: "Poppins",
-      ),
-      themeMode: ThemeMode.dark,
-      home: const NotesView(),
     );
   }
 }
